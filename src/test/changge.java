ab68e923d20bf02d0c85b0da7c777d777a4a3f02
xhyzkk <18710136860@163.com>
2018-04-10 20:46:29 +0800

    test

---
+++ src/test/changge.java
@@ -0,0 +1,20 @@
+package test;
+
+import java.io.File;
+
+public class changge {
+	
+	public static void main(String[] args) {
+		
+		
+		String url = "D:\\Ó¢ÐÛÊ±¿Ì\\799635929\\VideoData\\1466613396722";
+		for(int i = 0;i<10;i++) {
+			String url1 = url+"/"+i;
+			File fi = new File(url1);
+			File f2 = new File(url+"/"+i+".ts");
+			fi.renameTo(f2);
+		}
+		
+	}
+
+}
